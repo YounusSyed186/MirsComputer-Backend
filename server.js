9b0c75932dedd2f194a68d9050a6d7164469b46f
YounusSyed186 <younussyed1011@gmail.comgit config --global user.email younussyed1011@gmail.com>
2025-06-28 14:04:40 +0530

    ...

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const allowedOrigins = [
   'https://mirscomputers.vercel.app',
-  'https://mirscomputers-*-younus-syeds-projects.vercel.app/', // Add your preview deployment
+  'https://mirscomputers-efayg4smf-younus-syeds-projects.vercel.app/', // Add your preview deployment
   'http://localhost:3000', // Optional for local dev
 ];
 app.use(cors({
